8efcd1075012056630a77f4d8b741b47b1434deb
pergy-macos <persa.gyorgy@mistems.hu>
2019-09-03 15:45:11 +0200

    invalidate bounds uses rwhv's backing

--- atom/browser/osr/osr_host_display_client.h
+++ atom/browser/osr/osr_host_display_client.h
@@ -57,10 +57,6 @@ class OffScreenHostDisplayClient : public viz::HostDisplayClient {
   ~OffScreenHostDisplayClient() override;
 
   void SetActive(bool active);
-  // Theses are accesible from osr_rwhv, but currently unused (bc invalidate
-  // requests redraw)
-  const void* GetPixelMemory() const;
-  SkImageInfo GetPixelInfo() const;
 
  private:
   void IsOffscreen(IsOffscreenCallback callback) override;
